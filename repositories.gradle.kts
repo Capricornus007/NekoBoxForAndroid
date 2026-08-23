6892860bc1e8456a4a64a88a78e9442ee1fb319f
那個呆萌在山外 <5h1n38r16h7.d14m0nd@gmail.com>
2026-08-23 18:17:02 +0900

    fix: prefer official repositories before mirrors

--- repositories.gradle.kts
+++ repositories.gradle.kts
@@ -1,8 +1,8 @@
 repositories {
-    maven { url = uri("https://maven.aliyun.com/repository/google") }
-    maven { url = uri("https://maven.aliyun.com/repository/central") }
-    maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
     google()
     mavenCentral()
     gradlePluginPortal()
+    maven { url = uri("https://maven.aliyun.com/repository/google") }
+    maven { url = uri("https://maven.aliyun.com/repository/central") }
+    maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
 }
