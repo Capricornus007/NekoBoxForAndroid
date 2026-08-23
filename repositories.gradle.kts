6c285044cccdfcbdeeaabba75dee06a75a8dc86c
那個呆萌在山外 <5h1n38r16h7.d14m0nd@gmail.com>
2026-08-23 22:42:46 +0900

    build: use mirror for Media3 artifacts

--- repositories.gradle.kts
+++ repositories.gradle.kts
@@ -1,4 +1,18 @@
 repositories {
+    // dl.google.com intermittently terminates TLS handshakes for Media3 from
+    // some networks. Route only that group through the verified mirror; all
+    // other artifacts continue to prefer their official repositories below.
+    exclusiveContent {
+        forRepository {
+            maven {
+                name = "AliyunGoogleMedia3"
+                url = uri("https://maven.aliyun.com/repository/google")
+            }
+        }
+        filter {
+            includeGroup("androidx.media3")
+        }
+    }
     google()
     mavenCentral()
     gradlePluginPortal()
